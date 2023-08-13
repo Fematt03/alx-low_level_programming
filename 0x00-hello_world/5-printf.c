61fdedc9a61ecde8b079079c6339d6589f74b2e9
Femi Endurance Matthew <femattjunior@gmail.com>
2023-08-13 12:23:17 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * return: Always 0 (Success) 
+ * return: Always 0(Success) 
  */
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
